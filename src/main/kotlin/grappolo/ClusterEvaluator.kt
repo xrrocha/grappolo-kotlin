16c895db44841988257cb90edd5bba270a2a61f3
Ricardo Rocha <ricardo@xrrocha.net>
2020-05-15 23:30:08 -0400

    Now evaluating cluster quality based on similarity (not distance)

--- src/main/kotlin/grappolo/ClusterEvaluator.kt
+++ src/main/kotlin/grappolo/ClusterEvaluator.kt
@@ -11,40 +11,39 @@ object SimpleClusterEvaluator : ClusterEvaluator {
 
     private val logger = getLogger(this)
 
-    // TODO Compute evaluation based on similarity, not distance
     override fun evaluate(clusters: List<Set<Index>>, similarityMatrix: SimilarityMatrix): Evaluation {
 
-        val maxIntraClusterDistance =
+        val minIntraClusterSimilarity =
             clusters
                 .flatMap { clusterSet ->
                     val cluster = clusterSet.toList()
                     cluster.indices.flatMap { i ->
                         (i + 1 until cluster.size)
-                            .map { j -> 1.0 - similarityMatrix[cluster[i]][cluster[j]] }
-                            .filterNot { it == 1.0 }
+                            .map { j -> similarityMatrix[cluster[i]][cluster[j]] }
+                            .filterNot { it == 0.0 } // TODO Address lossy minSimilarity :-(
                     }
                 }
-                .max()!!
+                .min() ?: 0.0
 
-        fun clusterDistance(cluster1: Set<Index>, cluster2: Set<Index>): Similarity =
+        fun clusterSimilarity(cluster1: Set<Index>, cluster2: Set<Index>): Similarity =
             cluster1
                 .flatMap { i ->
                     cluster2.map { j ->
-                        1.0 - similarityMatrix[i][j]
+                        similarityMatrix[i][j]
                     }
                 }
                 .average()
 
-        val minInterClusterDistance =
+        val maxInterClusterSimilarity =
             clusters.indices.flatMap { i ->
                 (i + 1 until clusters.size).map { j ->
-                    clusterDistance(clusters[i], clusters[j])
+                    clusterSimilarity(clusters[i], clusters[j])
                 }
             }
-                .min() ?: 0.0
+                .max() ?: 1.0
 
-        val minValue = min(minInterClusterDistance, maxIntraClusterDistance)
-        val maxValue = max(minInterClusterDistance, maxIntraClusterDistance)
+        val minValue = min(maxInterClusterSimilarity, minIntraClusterSimilarity)
+        val maxValue = max(maxInterClusterSimilarity, minIntraClusterSimilarity)
         val evaluation = 1.0 - minValue / maxValue
         // logger.debug("minInterClusterDistance: $minInterClusterDistance, maxIntraClusterDistance: $maxIntraClusterDistance, evaluation: $evaluation")
 
